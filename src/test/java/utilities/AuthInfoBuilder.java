78f1cefdcd7b68559c5ba4457a1a52c17cec2275
Kanchan Basudkar <kanchan.basudkar@gmail.com>
2023-11-20 15:00:45 -0500

    code changes for AuthInfo

---
+++ src/test/java/utilities/AuthInfoBuilder.java
@@ -0,0 +1,32 @@
+package utilities;
+
+import com.dietician.backend.model.UserLoginRequest;
+import com.google.gson.Gson;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+
+import java.util.Properties;
+
+import static io.restassured.RestAssured.given;
+
+public class AuthInfoBuilder {
+
+    ConfigReaderAndWriter configReaderObj;
+    Properties prop;
+    public AuthInfoBuilder() {
+        configReaderObj = new ConfigReaderAndWriter();
+        prop = configReaderObj.init_prop();
+    }
+
+    public String getAuthInformation(){
+        String apiEndPointUri=prop.getProperty("BASE_URL").concat("/").concat("login");
+        UserLoginRequest userLoginRequest = new UserLoginRequest();
+        userLoginRequest.setUserLoginEmail(prop.getProperty("email"));
+        userLoginRequest.setPassword(prop.getProperty("password"));
+        Gson gson = new Gson();
+        String request = gson.toJson(userLoginRequest);
+        Response response = given().body(request).contentType("application/JSON").post(apiEndPointUri);
+        JsonPath jpath = new JsonPath(response.getBody().asString());
+        return jpath.getString("token");
+    }
+}
