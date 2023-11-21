cc65a3f607f983b31185b0adda545e26988b1371
Kanchan Basudkar <kanchan.basudkar@gmail.com>
2023-11-21 10:18:26 -0500

    update Jenkinsfile

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -12,6 +12,7 @@ import org.junit.runner.RunWith;
        // tags = "@EndtoEnd",
         monochrome = true,
         plugin = {"pretty",
+                "html:target/cucumber-html-report", "json:target/cucumber.json",
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
                 "pretty","io.qameta.allure.cucumber7jvm.AllureCucumber7"
                         + "Jvm"}
